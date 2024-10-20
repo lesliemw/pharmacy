503dd3d7405ce6f5bc651316ce66ff55e2440363
lesliemw <l.marie1598@gmail.com>
2024-10-20 21:55:55 +0100

    add stock data

---
+++ app/lib/stockData.jsx
@@ -0,0 +1,35 @@
+export const headers = [
+  "Drug Name",
+  "Strength",
+  "Brand",
+  "Pack Size",
+  "Price",
+  "Stock",
+];
+
+export const stockData = [
+  {
+    name: "Rosuvastatin",
+    strength: "5MG",
+    brand: "Crestor",
+    packSize: 28,
+    price: 2,
+    stock: 56,
+  },
+  {
+    name: "Rosuvastatin",
+    strength: "10MG",
+    brand: "Crestor",
+    packSize: 28,
+    price: 3,
+    stock: 84,
+  },
+  {
+    name: "Rosuvastatin",
+    strength: "20MG",
+    brand: "Crestor",
+    packSize: 28,
+    price: 4,
+    stock: 112,
+  },
+];
